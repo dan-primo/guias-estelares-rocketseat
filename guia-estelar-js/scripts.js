44faf5166a6d01d5ba179b3b3c725203490ada17
dan-primo <danielly.primo@gmail.com>
2021-01-24 13:37:39 -0300

    guias-estelares-http-jsParte1-2-3-4

---
+++ guia-estelar-js/scripts.js
@@ -0,0 +1,77 @@
+// console.log('Bem-vindos ao Starter');
+
+/*
+Cadeia de Caracteres
+
+    "" aspas duplas
+    '' aspas simples
+    `` template literals ou template strings
+*/
+
+// console.log(`1 + 1 é igual a ${1 + 1}.`)
+
+/*
+Numeros
+
+    33 // inteiros
+    12.5 // reais - float
+    NaN // Not a Number
+    Infinity // infinito
+*/
+
+/*
+Boolean
+True = Verdadeiro | False = Falso
+*/
+
+/*
+Undefined VS Null
+
+    undefined = indefinido
+
+    Null = Nulo, Objeto que não possui nada dentro (vazio), diferente de indefinido
+*/
+
+/*
+Object:
+    - Objeto
+    - Propriedades / Atributos
+    - Funcionalidades / Métodos
+
+    { propriedade: "valor" }
+*/
+
+/*
+Array (Vetores)
+
+    - Uma lista
+    - Agrupamento de dados
+    ["Anos", 200]
+*/
+
+/*
+Conclusão | Tipos de Dados
+
+Conforme o EcmaScript standard temos 9 tipos de dados:
+
+* Data types: Primitive / Primitive value ; Structural ; Structural Primite
+
+## Primitivos:
+
+    - String
+    - Number
+    - Boolean
+    - undefined
+    - Symbol
+    - BigInt
+
+## Estruturais
+
+    - Object: Array, Map, Set, Date...
+    - Function
+
+## Primitivo Estrutural / Structural Root Primitive
+- null
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+*/
---
+++ guia-estelar-js/variaveis.js
@@ -0,0 +1,59 @@
+// # Variáveis
+
+// - Nomes simbólicos para receber algum valor
+// - Atalhos de código
+// - Temos três palavras reservadas para criar variáveis em JS:
+//     var | let | const
+
+// var clima = "Quente"
+// console.log(clima)
+
+// let climaAtual = "Chuvoso"
+// console.log(climaAtual)
+
+// const climaSaoPaulo = "Frio"
+// console.log(climaSaoPaulo)
+
+// # Constantes são imutáveis
+
+/*
+    O Js é uma linguagem fracamente tipada e dinâmica
+    - Variáveis não precisam ter um tipo previamente definido;
+    - Podemos mudar o conteúdo da variável;
+    - Com exceção das constantes que são imutáveis.
+    - var é global e, também local | hoisting
+*/
+
+// # Scope | Escopo:
+// * Escopo determina a visibilidade de alguma variável no JS
+
+// Block statement é uma declaração de bloco;
+// var é global e local
+
+// - const e let são locais e só funcionam no escopo onde
+// elas foram criadas.
+
+/*
+
+==== Para criar nomes ====
+    - JS é case-sensitive (sensível ao caso)
+    - JS aceita a cadeia de caracteres Unicode
+
+    Podemos:
+    - Inciar c/ esses caracteres especiais: $
+    - Iniciar com letras
+    - Colocar acentos
+    - Letras maíusculas e minúsculas fazem a diferença
+
+    Não podemos:
+    - Iniciar com números
+    - Colocar espações vazios no nome
+
+    Boas práticas:
+    - Criar nomes que fazem sentido
+    - Que explique o que a variável é ou faz
+    - camelCase
+    - snake_case
+    - Escrever em inglês
+
+*/
