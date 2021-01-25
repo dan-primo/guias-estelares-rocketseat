159bb6190f79ae14a1093433b042f8a604dfcbbf
dan-primo <danielly.primo@gmail.com>
2021-01-24 23:02:07 -0300

    part5

---
+++ guia-estelar-js/praticando.js
@@ -0,0 +1,47 @@
+// assigment or atribuição de valores
+nome = "Maria"
+
+// que tipo de dado foi colocado na variável
+// console.log(typeof name)
+
+// agrupamento de declarações
+let age, isNotABot
+
+age = 90
+isNotABot = true
+
+// multiplos argumentos na funcao
+// console.log(nome, age, isNotABot)
+
+// escrita de texto + variáveis
+
+// concatenando valores
+//console.log('A ' + nome + ' tem ' + age + ' anos.')
+
+// interpolação de valores | template strings or template literals
+// nomeNovo = 'Maria Bonita'
+// console.log(`A ${nomeNovo} tem ${age} anos.`)
+
+/* Objects
+
+const person = {
+    nameNew: 'Martin',
+    ageNew: 40,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(person.nameNew)
+*/
+
+// Arrays
+
+const animals = [
+    'Cat',
+    'Dogs',
+    'Parrots'
+]
+
+// como acessa valores dentro do array?
+console.log(animals[1])
+console.log(animals.length)
