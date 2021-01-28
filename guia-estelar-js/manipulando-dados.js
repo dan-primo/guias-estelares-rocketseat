d3cc045a7c173745ae6b70c3021606590970eb22
dan-primo <danielly.primo@gmail.com>
2021-01-28 10:07:04 -0300

    part7

---
+++ guia-estelar-js/manipulando-dados.js
@@ -0,0 +1,36 @@
+// Prototype
+
+//  * prototype-based language
+//  * prototype chain
+//  * __proto__
+
+/*
+    Type conversion (typecasting) vs Type coersion
+
+    * Alteração de um tipo de dado para outro tipo
+
+    console.log('9' + 5) // -> coersion
+    console.log(Number('9') + 5) // -> conversion
+*/
+
+/*
+ Manipulando Strings e Números
+ Transformar String em Número e Número em String
+
+ let string = '123'
+Number(string)
+let number = 321
+String(number)
+*/
+
+/*
+    Manipulando Strings e Números
+
+    Contar quantos caracteres tem uma palavra e quantos
+    dígitos tem um número
+
+let word = "Paralelepido"
+console.log(word.length)
+let number = 1234
+console.log(String(number).length)
+*/
