778e3acc5cc1967989fff46d1865e967af7cfe5d
dan-primo <danielly.primo@gmail.com>
2021-02-17 21:46:04 -0300

    notas

---
+++ guia-estelar-js/sistema-notas-escolares.js
@@ -0,0 +1,14 @@
+/*
+
+# Transformar notas escolares
+
+Crie um algoritmo que transforma as notas do sistema
+numérico para sistema de notas em caracteres tipo: A, B e C
+
+* acima de 90 = A
+* entre 80 - 89 = B
+* entre 70 - 79 = C
+* entre 60 - 69 = D
+* abaixo de 60 = F
+
+*/
